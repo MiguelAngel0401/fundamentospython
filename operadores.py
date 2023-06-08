6b728078205154cd7da4b5e678616a28871e6403
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-06-08 14:43:24 -0600

    03 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#Operadores Matematicos#
+# + - * %#
+#/ Division  flotante#
+#//7 Division entera#
+#** Potencia#
+
+print(24+62)
+print(50-32)
+print(35*2)
+print(10%3)
+print(10/3)
+print(10//3)
+print(3**3)
+
+#Operadores String#
+#+ para concatenar#
+#* replicar un string#
+print("Hola a" + "todos!")
+print("DSM 3A " * 4)
