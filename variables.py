7c9d148f54b469801af417406c1bbcbdb5ba63b2
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-06-08 14:32:56 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es No tipado#
+name="Miguel Angel"
+age= 19
+is_single=False
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola soy Miguel Angel tengo 19 años y soltero=False
+"""
+
+#Utilizando format string#
+print("Hola soy {}, tengo {} años y soltero={}". format(name,age,is_single))
+print(f"Hola soy {name}, tengo {age} años y soltero={is_single}")
