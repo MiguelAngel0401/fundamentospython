c7e5ec43a15bfd7acdc09440180f55b0478d6d1a
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-06-14 12:35:43 -0600

    05 Condicionales

---
+++ operadores-2.py
@@ -0,0 +1,26 @@
+# Operadores booleanos
+print("Operadores Booleanos")
+# < <=
+print(10<10)
+print(10<=10)
+# > >=
+print(10>10)
+print(10>=10)
+# ==
+print(10==10)
+# !=
+print(10!=10)
+
+#Operadores logicos
+print("Operadores Logicos")
+# and (True if all true)
+print(True and True)
+print(True and False)
+
+#or (True if at least one True)
+print(False or False)
+print(False or True)
+
+# not true
+print(not True)
+print(not False)
