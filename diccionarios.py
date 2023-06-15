fd6741405f7f16547009fe286804ffeeffafb7a0
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-06-15 13:39:27 -0600

    10 Diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,18 @@
+#Diccionarios
+#{"Key: "value", "key": "value"}
+
+team ={
+    "name":"city",
+    "country":"England",
+    "champions":1,
+    "players":['Halland', 'greanlinsh']
+}
+
+print(type(team))
+print(type)
+print(team["name"])
+print(team[ "players"])
+team["players"].append("Kevin")
+team["name"]="Manchester city"
+team["leage"]= "Premiere league"
+print(team)
