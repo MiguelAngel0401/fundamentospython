36fa13808aeafb30a9088530e4dd3a964bd66b53
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-06-15 13:26:35 -0600

    09 Tuplas Y Sets

---
+++ tuplas_sets.py
@@ -0,0 +1,30 @@
+#Mas colecciones en Python
+
+#Tuplas
+#(item1,item2,item3)
+#Listas son mutables
+#Tuplas inmutables
+
+my_tuple=(1, "dos",3.1,True)
+print(type(my_tuple))
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[-1])
+#Error
+#my_tuple[0]="Algo mas"
+
+#Conjuntos Sets
+#Desordenado
+#Mutable
+#No permite duplicados
+
+my_set={"uno","dos","tres","uno"}
+print(type(my_set))
+print(my_set)
+my_set.add(4)
+print(my_set)
+
+a={1,2,3,4}
+b={3,4,5,6}
+print(a.union(b))
+print(a.intersection(b))
